9514c38311afae9acd4f01f4c11ae1c27990f3d9
refy1 <refy1k@ya.ru>
2026-04-04 18:15:39 +0500

    redesign main page

---
+++ CivilianModule/Models/BannerItem.cs
@@ -0,0 +1,7 @@
+﻿public class BannerItem
+{
+    public string Image { get; set; }
+    public string Title { get; set; }
+    public string Action { get; set; } // URL или название страницы
+    public bool IsExternal { get; set; }
+}
--- CivilianModule/Pages/MainPages/StartPageNoAuth.xaml.cs
+++ CivilianModule/Pages/MainPages/StartPageNoAuth.xaml.cs
@@ -1,17 +1,90 @@
-using CivilianModule.Pages.Other;
-using CivilianModule;
+using System.Collections.ObjectModel;
 
 namespace CivilianModule.Pages.MainPages;
 
 public partial class StartPageNoAuth : ContentPage
 {
-	public StartPageNoAuth()
-	{
-		InitializeComponent();
-	}
-
-    private async void Button_Auth(object sender, EventArgs e)
+    public ObservableCollection<BannerItem> Banners { get; set; }
+    public StartPageNoAuth()
     {
-        await Navigation.PushAsync(new MainPage());
+        InitializeComponent();
+
+        Banners = new ObservableCollection<BannerItem>
+        {
+            new BannerItem
+            {
+                Image = "max_banner.png",
+                Title = "Канал в MAX",
+                Action = "https://max.ru/id5918217655_gos",
+                IsExternal = true
+            },
+            new BannerItem
+            {
+                Image = "banner2.png",
+                Title = "Обращения граждан",
+                Action = "AppealsPage",
+                IsExternal = false
+            }
+        };
+
+        BindingContext = this;
+    }
+
+    private async void OnBannerTapped(object sender, TappedEventArgs e)
+    {
+        if (sender is not Border border)
+            return;
+
+        if (border.BindingContext is not BannerItem banner)
+            return;
+
+        if (banner.IsExternal)
+        {
+            await Launcher.Default.OpenAsync(new Uri(banner.Action));
+        }
+        else
+        {
+            await DisplayAlert("Навигация", $"Открыть страницу: {banner.Action}", "OK");
+        }
+    }
+
+    private async void OnLoginClicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("Авторизация", "Здесь позже будет переход на страницу входа.", "OK");
+    }
+
+    private async void OnBannerOpenClicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("Баннер", "Позже здесь будет переход на экран выбранной услуги.", "OK");
+    }
+
+    private async void OnAppealsClicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("Обращения", "Раздел обращений пока в разработке.", "OK");
+    }
+
+    private async void OnDocumentsClicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("Документы", "Раздел документов пока в разработке.", "OK");
+    }
+
+    private async void OnHousingClicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("ЖКХ и услуги", "Раздел услуг пока в разработке.", "OK");
+    }
+
+    private async void OnTkoClicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("ТКО", "Раздел ТКО пока в разработке.", "OK");
+    }
+
+    private async void OnNewsClicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("Новости", "Позже здесь будет экран с новостями и объявлениями.", "OK");
+    }
+
+    private async void OnProfileClicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("Личный кабинет", "После авторизации здесь будет открыт профиль гражданина.", "OK");
     }
 }
